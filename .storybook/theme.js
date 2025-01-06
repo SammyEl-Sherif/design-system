04ef35e33a64a700e62bff3473df7cceccc202aa
Sammy El-Sherif <elsherifsammy@gmail.com>
2025-01-06 16:26:03 -0600

    updated theme logo url

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,6 +3,6 @@ import { create } from '@storybook/theming';
 export default create({
   base: 'dark',
   brandTitle: 'WorkPace',
-  brandUrl: 'https://github.com/work-pace',
+  brandUrl: 'https://github.com/work-pace/design-system',
   brandImage: './images/logo.png',
 });
