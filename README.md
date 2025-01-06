584b126ed86b062764f76c8746629a76cbbbd3b2
Sammy El-Sherif <elsherifsammy@gmail.com>
2025-01-06 16:44:42 -0600

    cleanup

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # WorkPace Design System
 
-Check out the [WorkPace Design System](https://workpace.dev/?path=/docs/configure-your-project--docs) to view our full documentation.
+Check out the [WorkPace Design System](https://workpace.dev) to view our full documentation.
--- src/components/Header/Header.stories.js
+++ src/components/Header/Header.stories.js
@@ -3,7 +3,7 @@ import { fn } from '@storybook/test';
 import Header from './';
 
 export default {
-  title: 'Common/Header',
+  title: 'Components/Header',
   component: Header,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
