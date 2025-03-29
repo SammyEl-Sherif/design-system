cc70a0bf8ed154e3bcb038badf9e8f2eb816d1a2
Sammy El-Sherif <elsherifsammy@gmail.com>
2025-03-29 16:10:50 -0500

    ✨ feat: WKPC-5 consumer updates
        NOTION: [WKPC-5](https://www.notion.so/work-pace/WKPC-5)
        BREAKING-CHANGE: no

--- consumer/src/pages/index.tsx
+++ consumer/src/pages/index.tsx
@@ -1,13 +1,21 @@
-import { Button } from '@workpace/design-system';
+import { Button, Select, Text } from '@workpace/design-system';
 import styles from './index.module.scss';
 
 export default function Home() {
   return (
     <div>
       <main>
-        <h1>Consumer</h1>
         <div className={styles.main}>
-          <Button>Testing</Button>
+          <h1>Consumer</h1>
+          <Button variant={'brand-primary'}>brand-primary</Button>
+          <Button variant={'brand-secondary'}>brand-secondary</Button>
+          <Button variant={'default-primary'}>Testing</Button>
+          <Button variant={'default-secondary'}>Testing</Button>
+          <Select label="selectable">
+            <option>yooo1</option>
+            <option>yooo2</option>
+          </Select>
+          <Text variant={'headline-display-emphasis'}>Nice</Text>
         </div>
       </main>
     </div>
